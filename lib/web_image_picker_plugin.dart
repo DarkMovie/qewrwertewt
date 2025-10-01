8f3dbf00434cc3aa3916d078c9ff4b3679d318fe
DarkMovie <151950017+DarkMovie@users.noreply.github.com>
2025-10-01 19:47:45 +0330

    Delete lib directory

--- lib/web_image_picker_plugin.dart
+++
@@ -1,8 +0,0 @@
-// lib/web_image_picker_plugin.dart
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-class WebImagePickerPlugin {
-  static void registerWith(Registrar registrar) {
-    // ثبت پلاگین برای وب
-  }
-}
